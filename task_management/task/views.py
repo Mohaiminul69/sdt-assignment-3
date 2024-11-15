b940c9e949c829a6aafb28cfc7ca0dda01ea469a
mohaiminul69 <mohaiminul69@gmail.com>
2024-11-15 16:03:43 +0600

    edit delete done.

--- task_management/task/views.py
+++ task_management/task/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from . import forms
+from . import models
 
 
 # Create your views here.
@@ -12,3 +13,21 @@ def add_task(request):
     else:
         task_form = forms.TaskForm()
     return render(request, "add_task.html", {"form": task_form})
+
+
+def edit_task(request, id):
+    task = models.Task.objects.get(pk=id)
+    task_form = forms.TaskForm(instance=task)
+    if request.method == "POST":
+        task_form = forms.TaskForm(request.POST, instance=task)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect("homepage")
+
+    return render(request, "add_task.html", {"form": task_form})
+
+
+def delete_task(request, id):
+    task = models.Task.objects.get(pk=id)
+    task.delete()
+    return redirect("homepage")
